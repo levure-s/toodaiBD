de542e5b4094b0968b8705dbe9d63b36264aec9a
levure-s <levure.sauvage@gmail.com>
2021-08-02 08:44:49 +0900

    add_linkjs

---
+++ link.js
@@ -0,0 +1,23 @@
+var link = [
+    'https://twitter.com/moe__021/status/1421486086518317059',
+    'https://twitter.com/ame_nigate/status/1421488426017120258',
+    'https://twitter.com/klee34293156/status/1421489182401171459',
+    'https://twitter.com/hatomo2192/status/1421485813561319435',
+    'https://twitter.com/hatomo2192/status/1421486560814403589',
+    'https://twitter.com/aramaho_yt/status/1421485885548220421',
+    'https://twitter.com/marufoy430/status/1421485837720571906',
+    'https://twitter.com/ZUMI327/status/1421493284262711299',
+    'https://twitter.com/ZUMI327/status/1421493290348605441',
+    'https://twitter.com/OuSkNySo_1116/status/1421485825913548800',
+    'https://twitter.com/KnwRwi/status/1421487080874516484',
+    'https://twitter.com/Uekid1/status/1421486208878661634',
+    'https://twitter.com/hamanasu42/status/1421837805848649733',
+    'https://twitter.com/mABxYpubO4HVZCP/status/1421493014501879820',
+    'https://twitter.com/mABxYpubO4HVZCP/status/1421522970535555075',
+    'https://twitter.com/seolis220/status/1421748717489508353',
+    'https://twitter.com/hinanon_1011/status/1421490479632322563',
+    'https://twitter.com/KikuKen_1112/status/1421487405052305408',
+    'https://twitter.com/mizusa_hi_nin/status/1421608807944265728',
+    'https://twitter.com/miyukiLiSA0512/status/1421485826190417929'
+
+]
